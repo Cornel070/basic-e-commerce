ca73b51f3913332e0feb190f869132a3673fee35
Ekupnse Abang <shupel16@gmail.com>
2020-11-09 22:37:37 +0100

    first commit of the ecommerce site

---
+++ README.md
@@ -0,0 +1,2 @@
+A Simple and very basic e-commerce site built with react
+Demo - https://corneltodo.handiman.ng/
---
+++ build/precache-manifest.edfb8bf6e91acd720948fbd350a5b379.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "61d8f7e872cf40bf54f6d9dba2e36a18",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c8f67e45ba43c8f050e3",
+    "url": "/static/css/2.98705a83.chunk.css"
+  },
+  {
+    "revision": "7cc3a0554f3f4a2b745f",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "c8f67e45ba43c8f050e3",
+    "url": "/static/js/2.c580cb03.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.c580cb03.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7cc3a0554f3f4a2b745f",
+    "url": "/static/js/main.eb0000c4.chunk.js"
+  },
+  {
+    "revision": "969ec6062bb99b0b9815",
+    "url": "/static/js/runtime-main.a54c6ea9.js"
+  }
+]);
